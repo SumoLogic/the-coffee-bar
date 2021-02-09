3e8348b26a061d83e13440abff9975f29a3f38fe
Mateusz Rumian <mrumian@sumologic.com>
2021-02-10 00:38:07 +0100

    Add sweets into bar

--- applications/python-the-coffee-bar-apps/src/bar/bar.py
+++ applications/python-the-coffee-bar-apps/src/bar/bar.py
@@ -1,42 +1,86 @@
 import logging as log
+
+from flask import Response
 import requests
 
 from src.bar.coffee_machine import COFFEES
 from src.common.http_server import HttpServer
 
 
-class Bar(HttpServer):
-    PRICES = {
-        'espresso': 2,
-        'cappuccino': 4,
-        'americano': 3
-    }
+PRICES = {
+    'espresso': 2,
+    'cappuccino': 4,
+    'americano': 3,
+    'cornetto': 1,
+    'cannolo_siciliano': 3,
+    'torta': 1.5,
+    'budini_fiorentini': 1.25,
+    'muffin_alla_ricotta': 2,
+    'tiramisu': 2.5,
+}
 
+
+def set_product_status(response: requests.models.Response, status_key: str):
+    product_status = {}
+
+    if response.status_code == 200:
+        product_status[status_key] = True
+    else:
+        product_status[status_key] = False
+
+    return product_status
+
+
+class Bar(HttpServer):
     def __init__(self, coffee_machine_host: str, coffee_machine_port: str, cashdesk_host: str, cashdesk_port: int,
-                 name: str = 'The Coffee Bar', host: str = 'localhost', port: int = 8082):
+                 name: str = 'The Coffee Bar', host: str = 'localhost', port: int = 8082, sweets_url: str = None):
         super().__init__(name, host, port)
         self.coffee_machine_host = coffee_machine_host
         self.coffee_machine_port = coffee_machine_port
         self.cashdesk_host = cashdesk_host
         self.cashdesk_port = cashdesk_port
+        self.sweets_url = sweets_url
         self.add_all_endpoints()
 
     def add_all_endpoints(self):
-        self.add_endpoint(endpoint='/do_espresso', endpoint_name='do_espresso',
-                          handler=self.do_espresso)
+        self.add_endpoint(endpoint='/order', endpoint_name='order',
+                          handler=self.order)
 
-    def do_espresso(self, data):
-        coffe_machine_url = 'http://{}:{}{}'.format(self.coffee_machine_host, self.coffee_machine_port,
-                                                    COFFEES['espresso'])
+    def order(self, data):
 
-        log.info('Send request to the coffee-machine')
-        coffe_status = requests.post(url=coffe_machine_url, json=data)
+        # OPTIONAL: If sweets_url is set, get sweets_status
+        sweets_status = {'sweets_status': False}
+        if self.sweets_url:
+            sweets_status = self.get_sweets(data=data)
+            data.update(sweets_status)
 
-        if coffe_status.status_code == 200:
-            data.update({'price': self.PRICES['espresso']})
+        # Get coffee_status
+        coffee_status = self.get_coffee(data=data)
+        data.update(coffee_status)
+
+        if coffee_status['coffee_status'] is True or sweets_status['sweets_status'] is True:
             return self.process_payment(data=data)
         else:
-            return coffe_status
+            result = {
+                'reason': 'Lack of requested products',
+                'products': [data['coffee'], data['sweets']],
+            }
+            return Response(result, status=404, mimetype='application/json')
+
+    def get_sweets(self, data):
+        log.info('Check if %s is available', data['sweets'])
+        response = requests.post(url=self.sweets_url, json=data)
+
+        return set_product_status(response=response, status_key='sweets_status')
+
+    def get_coffee(self, data):
+        log.info('Send request to the coffee-machine')
+        coffee_machine_url = 'http://{}:{}{}'.format(self.coffee_machine_host, self.coffee_machine_port,
+                                                     COFFEES['espresso'])
+
+        response = requests.post(url=coffee_machine_url, json=data)
+
+        return set_product_status(response=response, status_key='coffee_status')
 
     def process_payment(self, data):
         log.info('Send request to the cashdesk', data)
