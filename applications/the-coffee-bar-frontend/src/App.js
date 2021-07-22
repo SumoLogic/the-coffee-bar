a33545372f58accdd032c3d6e2f46a03f48233e6
Mateusz Rumian <mrumian@sumologic.com>
2021-07-22 18:01:42 +0200

    Create TCB Frontend

--- applications/the-coffee-bar-frontend/src/App.js
+++ applications/the-coffee-bar-frontend/src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import {
   ChakraProvider,
   Box,
+  Center,
   Text,
-  Link,
-  VStack,
-  Code,
   theme,
-  Image,
   Grid,
-  GridItem
+  Stack,
+  SimpleGrid,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
@@ -23,11 +21,24 @@ function App() {
       <Box textAlign="center" fontSize="xl">
         <Grid minH="10vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text size='h1' fontSize='xxx-large'>Order your &#x2615; and &#x1F36A;</Text>
-            <OrderForm/>
-          </VStack>
+          <Center>
+            <Stack isInline>
+              <Box>
+                <Logo h="40vmin" pointerEvents="none" />
+              </Box>
+              <SimpleGrid columns={1} rows={2} spacingY='10px'>
+                <Box height='1px'>
+                  <Text size='h1' fontSize='xxx-large' style={{ textAlign: 'left' }}>The Sumo Logic Coffee Bar</Text>
+                </Box>
+                <Box>
+                  <Text size='h2' fontSize='xx-large' style={{ textAlign: 'left' }}>Welcome in the Sumo Logic Coffee
+                    Bar.<br />You will find here best brewed coffee<br /> and the most delicious confectionery in the
+                    world.<br />Take your time and order your &#x2615; and &#x1F36A;.</Text>
+                </Box>
+              </SimpleGrid>
+            </Stack>
+          </Center>
+          <OrderForm />
         </Grid>
       </Box>
     </ChakraProvider>
