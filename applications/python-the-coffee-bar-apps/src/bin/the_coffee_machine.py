91ec3182ecdebe6994c6099a6caa5f7504cbf654
Mateusz Rumian <mrumian@sumologic.com>
2021-10-28 15:45:00 +0200

    Use cron to schedule cpu increase, bump deps

--- applications/python-the-coffee-bar-apps/src/bin/the_coffee_machine.py
+++ applications/python-the-coffee-bar-apps/src/bin/the_coffee_machine.py
@@ -5,6 +5,8 @@ from src.bar.coffee_machine import CoffeeMachine
 from src.utils.config import Config, args_to_conf_dict, merge_configuration
 from src.utils.logger import configure_logging
 
+from cron_descriptor import get_description
+
 
 def main():
     APP_NAME = 'The Coffee Machine'
@@ -15,7 +17,7 @@ def main():
     parser.add_argument('-m', '--machine-svc-host', type=str, help='Machine Service Host')
     parser.add_argument('-n', '--machine-svc-port', type=int, help='Machine Service Port')
     parser.add_argument('-c', '--config', type=str, help='Configuration file path')
-    parser.add_argument('-i', '--cpu-increase-interval', type=int, help='CPU increase interval [m]')
+    parser.add_argument('-i', '--cpu-increase-cron', type=str, help='CPU increase cron string')
     parser.add_argument('-d', '--cpu-increase-duration', type=int, help='CPU increase duration [m]')
     parser.add_argument('-t', '--cpu-increase-threads', type=int, help='Number of threads for CPU increaser')
     parser.add_argument('-l', '--log-level', type=str, help='Application log level', default='info')
@@ -33,10 +35,12 @@ def main():
 
     log.info('********** Starting %s **********', APP_NAME)
     log.info('Configuration: %s', configuration)
+    log.info('CPU Increaser CRON: %s', get_description(configuration['cpu_increase_cron']))
+
     coffee_machine = CoffeeMachine(name=APP_NAME, host=configuration['host'], port=configuration['port'],
                                    machine_svc_host=configuration['machine_svc_host'],
                                    machine_svc_port=configuration['machine_svc_port'],
-                                   cpu_increase_interval=configuration['cpu_increase_interval'],
+                                   cpu_increase_cron=configuration['cpu_increase_cron'],
                                    cpu_increase_duration=configuration['cpu_increase_duration'],
                                    cpu_increase_threads=configuration['cpu_increase_threads'])
     coffee_machine.run()
