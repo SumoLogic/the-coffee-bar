3079d0368fcb0b7b9696c7f6b98aa03c75e88810
Mateusz Rumian <mrumian@sumologic.com>
2021-11-18 23:12:43 +0100

    configure logger

--- applications/ruby-the-coffee-bar-apps/lib/coffee.rb
+++ applications/ruby-the-coffee-bar-apps/lib/coffee.rb
@@ -4,6 +4,7 @@ require 'rubygems'
 require 'bundler/setup'
 require 'sinatra'
 require 'json'
+require 'logger'
 
 require_relative "opentelemetry-instrumentation"
 require_relative "version"
@@ -14,38 +15,46 @@ class Coffee < Sinatra::Base
     host = ARGV[0] || 'coffee-svc'
     port = ARGV[1] || 9091
 
+    set :environment, :production
     set :bind, host
     set :port, port
-    puts "INFO - Starting Coffee Service: #{host}:#{port}"
+
+    configure do
+        logger = ::Logger.new(STDOUT)
+        logger.formatter = proc do | severity, time, progname, msg |
+            span_id = OpenTelemetry::Trace.current_span.context.hex_span_id
+            trace_id = OpenTelemetry::Trace.current_span.context.hex_trace_id
+            "#{time}, #{severity}: #{msg} - trace_id=#{trace_id} - span_id=#{span_id}\n"
+        end
+        set :logger, logger
+        use Rack::AccessLog, logger
+    end
+
+    logger.info "Starting Coffee Service: #{host}:#{port}"
 
     # For K8s livenessProbe
     get '/' do
-        span_id = OpenTelemetry::Trace.current_span.context.hex_span_id
-        trace_id = OpenTelemetry::Trace.current_span.context.hex_trace_id
-        puts "INFO - Received possible K8s LivnessProbe request - trace_id=#{trace_id} - span_id=#{span_id}"
+        logger.info "Received possible K8s LivnessProbe request"
         status 200
         body "I'm alive!"
     end
 
     post '/get_beans' do
-        span_id = OpenTelemetry::Trace.current_span.context.hex_span_id
-        trace_id = OpenTelemetry::Trace.current_span.context.hex_trace_id
-
         payload = JSON.parse(request.body.read)
-        puts "INFO - Received order for coffee grains #{payload['grains']} - trace_id=#{trace_id} - span_id=#{span_id}"
+        logger.info "Received order for coffee grains #{payload['grains']}"
 
         content_type :json
 
         if payload['grains'] < 1
             status 502
             body "Lack of coffee grains"
-            puts "ERROR - Lack of coffee grains in amount #{payload['grains']} - trace_id=#{trace_id} - span_id=#{span_id}"
+            logger.error "Lack of coffee grains in amount #{payload['grains']}"
 
             ## Add event into span
             OpenTelemetry::Trace.current_span.add_event("exception", attributes: { 'exception.code' => '502', 'exception.message' => 'Lack of coffee' })
         else
             body "Grains provided"
-            puts "INFO - Coffee grains in amount #{payload['grains']} provided - trace_id=#{trace_id} - span_id=#{span_id}"
+            logger.info "Coffee grains in amount #{payload['grains']} provided"
         end
     end
 
