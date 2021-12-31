629a75406b3ac18527c5b8ba8d5138de403fc18b
Mateusz Rumian <mrumian@sumologic.com>
2021-12-31 14:48:26 +0100

    Add network delay method

--- applications/python-the-coffee-bar-apps/src/utils/cpu_increaser.py
+++ applications/python-the-coffee-bar-apps/src/utils/cpu_increaser.py
@@ -1,13 +1,15 @@
 import logging as log
+import subprocess
 import time
 from multiprocessing.pool import Pool
+
 
 def magic_cpu_usage_increaser(period: int):
     start_time = time.time()
     while time.time() - start_time < period:
         pass
     log.error('Thread stuck in kernel module for %s seconds, waiting for file descriptor to be released.'
-            'Too many open files.', period)
+              'Too many open files.', period)
 
 
 def increase_cpu(period: int, threads: int):
@@ -17,3 +19,13 @@ def increase_cpu(period: int, threads: int):
         p.map(magic_cpu_usage_increaser, [period])
 
     log.info('Deploying new version 1.20.123')
+
+
+def network_delay(delay: str, period: int):
+    log.info('Adding network delay: %s' % delay)
+    subprocess.call(['tcset', 'eth0', '--delay', delay])
+
+    time.sleep(period)
+
+    log.info('Removing network delay')
+    subprocess.call(['tcdel', 'eth0', '--all'])
