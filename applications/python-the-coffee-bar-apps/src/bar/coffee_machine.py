f2fce286ca05708a7b95459ca1240152dea0c6e5
arunpatyal <arun@sumologic.com>
2021-12-31 17:05:54 +0530

    Changed threads to Processes.

--- applications/python-the-coffee-bar-apps/src/bar/coffee_machine.py
+++ applications/python-the-coffee-bar-apps/src/bar/coffee_machine.py
@@ -17,7 +17,7 @@ class CoffeeMachine(HttpServer):
     def __init__(self, name: str = 'The Coffee Machine', host: str = 'localhost', port: int = 8084,
                  machine_svc_host: str = 'localhost', machine_svc_port: int = 9090,
                  cpu_increase_cron: str = '0 * * * *', cpu_increase_start_date: str = None, 
-                 cpu_increase_duration: int = 60, cpu_increase_threads: int = 500):
+                 cpu_increase_duration: int = 60, cpu_increase_threads: int = 250):
 
         super().__init__(name, host, port)
         self.cpu_increase_cron = cpu_increase_cron
--- applications/python-the-coffee-bar-apps/src/utils/cpu_increaser.py
+++ applications/python-the-coffee-bar-apps/src/utils/cpu_increaser.py
@@ -1,31 +1,19 @@
 import logging as log
-import threading
 import time
+from multiprocessing.pool import Pool
 
-
-def magic_cpu_usage_increaser(period: int, thread_no: int, threads: int):
+def magic_cpu_usage_increaser(period: int):
     start_time = time.time()
     while time.time() - start_time < period:
         pass
-    log.error('Thread 0x[%s] stuck in kernel module for 5 seconds, waiting for file descriptor to be released.'
-              'Too many open files.', thread_no)
-
-    # When last thread ends perform "roll back"
-    if thread_no + 1 == threads:
-        log.info('Deploying new version 1.20.123')
+    log.error('Thread stuck in kernel module for %s seconds, waiting for file descriptor to be released.'
+            'Too many open files.', period)
 
 
 def increase_cpu(period: int, threads: int):
     log.info('Deploying new version 1.20.124')
     log.info('Upgrade initiated: admin mode by joe@sumocoffee.com')
-    thread_list = []
+    with Pool(threads) as p:
+        p.map(magic_cpu_usage_increaser, [period])
 
-    for i in range(1, threads):
-        t = threading.Thread(target=magic_cpu_usage_increaser, args=(period, i, threads))
-        thread_list.append(t)
-
-    for thread in thread_list:
-        thread.start()
-
-    for thread in thread_list:
-        thread.join()
+    log.info('Deploying new version 1.20.123')
