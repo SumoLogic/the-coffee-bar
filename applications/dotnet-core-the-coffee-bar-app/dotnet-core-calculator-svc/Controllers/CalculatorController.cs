c3955925c4acd8ce2443825a03152007e2d0598d
Mateusz Rumian <mrumian@sumologic.com>
2022-03-01 19:30:03 +0100

    Bump dotnet

--- applications/dotnet-core-the-coffee-bar-app/dotnet-core-calculator-svc/Controllers/CalculatorController.cs
+++ applications/dotnet-core-the-coffee-bar-app/dotnet-core-calculator-svc/Controllers/CalculatorController.cs
@@ -6,59 +6,58 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Linq;
 using OpenTelemetry.Trace;
 
-namespace dotnet_core_calculator_svc.Controllers
+namespace dotnet_core_calculator_svc.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CalculatorController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class CalculatorController : ControllerBase
+    private readonly ILogger<CalculatorController> _logger;
+
+    public CalculatorController(ILogger<CalculatorController> logger)
     {
-        private readonly ILogger<CalculatorController> _logger;
+        _logger = logger;
+    }
 
-        public CalculatorController(ILogger<CalculatorController> logger)
+    [HttpPost]
+    [Consumes("application/json")]
+    public async Task<JsonResult> Post()
+    {
+        var traceId = Tracer.CurrentSpan.Context.TraceId;
+        var spanId = Tracer.CurrentSpan.Context.SpanId;
+
+        JsonResult result;
+        using (var reader = new StreamReader(
+                   Request.Body,
+                   encoding: Encoding.UTF8,
+                   false
+               ))
         {
-            _logger = logger;
-        }
+            var bodyString = await reader.ReadToEndAsync();
 
-        [HttpPost]
-        [Consumes("application/json")]
-        public async Task<JsonResult> Post()
-        {
-            var traceId = Tracer.CurrentSpan.Context.TraceId;
-            var spanId = Tracer.CurrentSpan.Context.SpanId;
+            _logger.LogInformation("Payment calculation for " + bodyString +
+                                   " - trace_id=" + traceId +
+                                   " - span_id=" + spanId
+            );
 
-            JsonResult result;
-            using (var reader = new StreamReader(
-                Request.Body,
-                encoding: Encoding.UTF8,
-                detectEncodingFromByteOrderMarks: false
-            ))
+            var value = JObject.Parse(bodyString);
+            var productValue = value.GetValue("product").ToString();
+            var amountValue = value.GetValue("amount").ToObject<int>();
+            var priceValue = value.GetValue("price").ToObject<int>();
+            var totalValue = priceValue * amountValue;
+
+            _logger.LogInformation("Calculation done - " + amountValue + " * "
+                                   + priceValue + " = " + totalValue +
+                                   " - trace_id=" + traceId +
+                                   " - span_id=" + spanId);
+
+            result = new JsonResult(new
             {
-                var bodyString = await reader.ReadToEndAsync();
-
-                _logger.LogInformation("Payment calculation for " + bodyString +
-                                       " - trace_id=" + traceId +
-                                       " - span_id=" + spanId
-                );
-
-                var value = JObject.Parse(bodyString);
-                var productValue = value.GetValue("product").ToString();
-                var amountValue = value.GetValue("amount").ToObject<int>();
-                var priceValue = value.GetValue("price").ToObject<int>();
-                var totalValue = priceValue * amountValue;
-
-                _logger.LogInformation("Calculation done - " + amountValue + " * "
-                                       + priceValue + " = " + totalValue +
-                                       " - trace_id=" + traceId +
-                                       " - span_id=" + spanId);
-
-                result = new JsonResult(new
-                {
-                    product = productValue, amount = amountValue,
-                    price = priceValue, total = totalValue
-                });
-            }
-
-            return result;
+                product = productValue, amount = amountValue,
+                price = priceValue, total = totalValue
+            });
         }
+
+        return result;
     }
 }
--- applications/dotnet-core-the-coffee-bar-app/dotnet-core-calculator-svc/Controllers/IndexController.cs
+++ applications/dotnet-core-the-coffee-bar-app/dotnet-core-calculator-svc/Controllers/IndexController.cs
@@ -2,28 +2,28 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using OpenTelemetry.Trace;
 
-namespace dotnet_core_calculator_svc.Controllers
+namespace dotnet_core_calculator_svc.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class IndexController : Controller
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class IndexController : Controller
+    private readonly ILogger<IndexController> _logger;
+
+    public IndexController(ILogger<IndexController> logger)
     {
-        private readonly ILogger<IndexController> _logger;
-        
-        public IndexController(ILogger<IndexController> logger)
-        {
-            _logger = logger;
-        }
-        
-        [HttpGet][Route("/")]
-        public IActionResult Index()
-        {    
-            var traceId = Tracer.CurrentSpan.Context.TraceId;
-            var spanId = Tracer.CurrentSpan.Context.SpanId;
-            _logger.LogInformation("I'm alive! Possible K8s LivenessProbe request." +
-                                   " - trace_id=" + traceId +
-                                   " - span_id=" + spanId);
-            return Ok("I'm alive!");
-        }
+        _logger = logger;
+    }
+
+    [HttpGet]
+    [Route("/")]
+    public IActionResult Index()
+    {
+        var traceId = Tracer.CurrentSpan.Context.TraceId;
+        var spanId = Tracer.CurrentSpan.Context.SpanId;
+        _logger.LogInformation("I'm alive! Possible K8s LivenessProbe request." +
+                               " - trace_id=" + traceId +
+                               " - span_id=" + spanId);
+        return Ok("I'm alive!");
     }
 }
