fbb4677e9d87c084e0d8488d200342e6ed14bc91
Mateusz Rumian <mrumian@sumologic.com>
2022-05-06 10:49:53 +0200

    chore(lambda): cleanup

--- applications/aws-lambdas/functions/cakes/cakes.py
+++ applications/aws-lambdas/functions/cakes/cakes.py
@@ -1,15 +1,11 @@
 import json
-import boto3
 
 from opentelemetry import trace
 
 AVAILABLE_CAKES = ['cornetto', 'cannolo_siciliano', 'torta', 'muffin']
-s3 = boto3.resource('s3')
 
 
-def is_avi(data):
-    cake = str(data['cakes'])
-    context = data['link_context']
+def is_avi(cake, context):
     span_context = trace.SpanContext(context['trace_id'],
                                      context['span_id'],
                                      context['is_remote'],
@@ -24,7 +20,6 @@ def is_avi(data):
             span.set_attribute('is_avi', True)
             status = 200
         else:
-            span.set_attribute('requested_cake', cake)
             span.set_attribute('is_avi', False)
             status = 404
 
@@ -33,11 +28,14 @@ def is_avi(data):
 
 def lambda_handler(event, context):
     data = json.loads(event)
-    result = is_avi(data)
+    cake = data['cakes']
+    link_context = data['link_context']
+
+    result = is_avi(cake, link_context)
 
     if result == 200:
-        print('%s is available' % data['cakes'])
-        return {'statusCode': 200, 'body': {'cakes': data['cakes']}}
+        print('%s is available' % cake)
+        return {'statusCode': 200, 'body': {'cakes': cake}}
     else:
         print('%s is not available' % data['cakes'])
-        return {'statusCode': 404, 'body': {'cakes': data['cakes']}}
+        return {'statusCode': 404, 'body': {'cakes': cake}}
