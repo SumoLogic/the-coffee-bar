202c7b3db0427f739f48a6dcaaafb9e39a95ab66
Mateusz Rumian <mrumian@sumologic.com>
2022-08-16 14:34:13 +0200

    chore(rubyapps): configure only instrumentation

--- applications/ruby-the-coffee-bar-apps/lib/opentelemetry-instrumentation.rb
+++ applications/ruby-the-coffee-bar-apps/lib/opentelemetry-instrumentation.rb
@@ -5,9 +5,4 @@ Bundler.require
 
 OpenTelemetry::SDK.configure do |c|
     c.use_all
-    c.add_span_processor(
-        OpenTelemetry::SDK::Trace::Export::BatchSpanProcessor.new(
-          OpenTelemetry::Exporter::OTLP::Exporter.new
-        )
-      )
 end
