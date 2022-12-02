f8192dfd0a9473a528f9de077c1ca0decb2e08a8
soagarwal07 <soagarwal@sumologic.com>
2022-12-02 16:39:06 +0530

    removed conflicts

--- applications/python-the-coffee-bar-apps/src/bar/bar.py
+++ applications/python-the-coffee-bar-apps/src/bar/bar.py
@@ -113,11 +113,8 @@ class Bar(HttpServer):
         log.info('Send request to the cashdesk', data)
         cashdesk_url = 'http://{}:{}{}'.format(self.cashdesk_host, self.cashdesk_port, '/pay_in')
         try:
-<<<<<<< HEAD
-            cashdesk_status = requests.post(url=cashdesk_url, json=data, timeout=2)
-=======
+
             cashdesk_status = requests.post(url=cashdesk_url, json=data, timeout=10)
->>>>>>> main
         except requests.Timeout:
             return make_response({'reason': 'Request Timeout'}, 504)
         except requests.ConnectionError:
